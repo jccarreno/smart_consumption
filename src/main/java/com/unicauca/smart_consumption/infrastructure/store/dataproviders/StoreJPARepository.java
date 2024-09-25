6c2d42126b8609784ab8737fa76096c601c56d3c
Juan Camilo Carreno Ramirez <128004050+jccarreno@users.noreply.github.com>
2024-09-25 16:32:11 -0500

    Delete src/main/java/com/unicauca/smart_consumption/infrastructure/store directory

--- src/main/java/com/unicauca/smart_consumption/infrastructure/store/dataproviders/StoreJPARepository.java
+++
@@ -1,9 +0,0 @@
-package com.unicauca.smart_consumption.infrastructure.store.dataproviders;
-
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StoreJPARepository  extends JpaRepository<StoreJPAEntity, String> {
-
-}
