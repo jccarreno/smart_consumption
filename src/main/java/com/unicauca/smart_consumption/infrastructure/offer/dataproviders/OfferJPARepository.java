dc00d19a677e2ce998de0435936e64be3232c13f
Juan Camilo Carreno Ramirez <128004050+jccarreno@users.noreply.github.com>
2024-09-25 16:31:32 -0500

    Delete src/main/java/com/unicauca/smart_consumption/infrastructure/offer directory

--- src/main/java/com/unicauca/smart_consumption/infrastructure/offer/dataproviders/OfferJPARepository.java
+++
@@ -1,6 +0,0 @@
-package com.unicauca.smart_consumption.infrastructure.offer.dataproviders;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OfferJPARepository  extends JpaRepository<OfferJPAEntity,String> {
-}
